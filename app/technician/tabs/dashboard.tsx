2220034483a388531cf9082c06f3c3501065a828
mahesh patra <62048436+maheshpatra@users.noreply.github.com>
2023-10-12 01:28:42 +0530

    update
    update

--- app/technician/tabs/dashboard.tsx
+++ app/technician/tabs/dashboard.tsx
@@ -13,26 +13,9 @@ const dashboard = () => {
 
   const [activeTab, setActiveTab] = useState('Pending');
 
-  const data = [
-    { id: '1', title: 'Task 1', status: 'Pending' },
-    { id: '2', title: 'Task 2', status: 'In Progress' },
-    { id: '3', title: 'Task 3', status: 'Complete' },
-    // Add more tasks with different statuses
-  ];
+  const data = ['Pending', 'In Progress', 'Complete'];
 
-  const renderTasks = (status) => {
-    return (
-      <FlatList
-        data={data.filter((task) => task.status === status)}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <View style={{ padding: 10, borderBottomWidth: 1, borderBottomColor: '#ccc' }}>
-            <Text>{item.title}</Text>
-          </View>
-        )}
-      />
-    );
-  };
+
 
   return (
     <View style={{ flex: 1 }}>
@@ -46,30 +29,38 @@ const dashboard = () => {
         />
       </View>
 
-      <View style={{ flexDirection: 'row', alignSelf: 'center' }}>
-        <TouchableOpacity style={[styles.tab, { borderColor: activeTab === 'Pending' ? Colors.light.tint : 'black' }]} onPress={() => setActiveTab('Pending')}>
-          <Text style={[styles.tabText, { color: activeTab === 'Pending' ? Colors.light.tint : 'black' }]}>Pending</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={[styles.tab, { borderColor: activeTab === 'In Progress' ? Colors.light.tint : 'black' }]} onPress={() => setActiveTab('In Progress')}>
-          <Text style={[styles.tabText, { color: activeTab === 'In Progress' ? Colors.light.tint : 'black' }]}>In Progress</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={[styles.tab, { borderColor: activeTab === 'Complete' ? Colors.light.tint : 'black' }]} onPress={() => setActiveTab('Complete')}>
-          <Text style={[styles.tabText, { color: activeTab === 'Complete' ? Colors.light.tint : 'black' }]}>Complete</Text>
-        </TouchableOpacity>
+
+      <View>
+        <FlatList
+          data={data}
+          horizontal={true}
+          
+          style={{ alignSelf: 'center',marginBottom:20  }}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <TouchableOpacity style={[styles.tabt, { borderColor: activeTab === item ? Colors.light.tint : 'black' }]} onPress={() => setActiveTab(item)}>
+              <Text style={[styles.tabText, { color: activeTab === item ? Colors.light.tint : 'black' }]}>{item}</Text>
+            </TouchableOpacity>
+          )}
+        />
+
       </View>
 
-      <View style={{width:'80%',height:100,alignSelf:'center',marginTop:20,borderRadius:5,flexDirection:'row',alignItems:'center',borderWidth:1,borderColor:'#ccc'}}>
-        <View style={{height:60,width:60,marginHorizontal:ts*5,backgroundColor:'#ddd'}}>
+
+
+
+      <View style={{ width: '80%', height: 100, alignSelf: 'center', borderRadius: 5, flexDirection: 'row', alignItems: 'center', borderWidth: 1, borderColor: '#ccc' }}>
+        <View style={{ height: 60, width: 60, marginHorizontal: ts * 5, backgroundColor: '#ddd' }}>
 
 
         </View>
         <View>
-        <Text style={{fontSize:16,fontWeight:'bold'}}>Cleaning Service</Text>
-        <Text>Order No : 65382</Text>
-        <Text>Payment Mode : paytm</Text>
+          <Text style={{ fontSize: 16, fontWeight: 'bold' }}>Cleaning Service</Text>
+          <Text>Order No : 65382</Text>
+          <Text>Payment Mode : paytm</Text>
 
         </View>
-       
+
       </View>
 
 
@@ -84,11 +75,14 @@ export default dashboard;
 
 const styles = StyleSheet.create({
   tab: {
-    borderBottomWidth: 1, width: '25%', height: ts * 12, justifyContent: 'center', alignItems: 'center'
+    borderBottomWidth: 1, width: '30%', height: ts * 12, justifyContent: 'center', alignItems: 'center'
   },
   tabText: {
     color: '#555',
     fontWeight: 'bold',
     fontSize: 16
+  },
+  tabt: {
+    borderBottomWidth: 1, width: ts * 28, height: ts * 12, justifyContent: 'center', alignItems: 'center'
   }
 });
